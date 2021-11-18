9b94f6a03da8af71de22d46457163ab92d0ad15b
Vitor Melo <86443974+VitorAMelo@users.noreply.github.com>
2021-11-18 20:45:29 -0300

    atualizar script cadastro.js

--- Maskotti/script/scriptCadastro.js
+++ Maskotti/script/scriptCadastro.js
@@ -7,18 +7,18 @@ var transfer = async(nome, cpf, email, password, endereco, telefone) => {
 }
 
 function enviar() {
-    /*var nome = document.getElementById("nome").value;
+    var nome = document.getElementById("nome").value;
     var cpf = document.getElementById("cpf").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var endereco = document.getElementById("endereco").value;
-    var telefone = document.getElementById("telefone").value;*/
-    var nome = "jamir";
+    var telefone = document.getElementById("telefone").value;
+    /*var nome = "jamir";
     var cpf = "13912811601";
     var email = "jamirmelo7@gmail.com";
     var password = "090776";
     var endereco = "rua baco 365";
-    var telefone = "31982693558";
+    var telefone = "31982693558";*/
 
     transfer(nome, cpf, email, password, endereco, telefone)
     window.location.href = 'login.html'
